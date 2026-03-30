550ddd188eded2de451bf38b62dfcd8f4c924d8e
VysakhHQ <vysakhmethalayil@gmail.com>
2026-03-30 09:59:16 +0530

    UC2: Add Passenger Bogies using ArrayList Operations

---
+++ App/src/UC2PassengerBogies.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class UC2PassengerBogies{
+    public static void main(String[] args){
+        ArrayList<String> bogies=new ArrayList<>();
+        bogies.add("Sleeper");
+        bogies.add("AC Chair");
+        bogies.add("First Class");
+        System.out.println(bogies);
+        bogies.remove("AC Chair");
+        System.out.println(bogies.contains("Sleeper"));
+        System.out.println(bogies);
+    }
+}
