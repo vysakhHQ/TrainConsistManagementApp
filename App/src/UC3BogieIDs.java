015b0bb6635020fd75dd097b40abea8bdd735d23
VysakhHQ <vysakhmethalayil@gmail.com>
2026-03-30 10:06:12 +0530

    UC3: Track Unique Bogie IDs using HashSet

---
+++ App/src/UC3BogieIDs.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class UC3BogieIDs{
+    public static void main(String[] args){
+        HashSet<String> ids=new HashSet<>();
+        ids.add("BG101");
+        ids.add("BG102");
+        ids.add("BG101");
+        ids.add("BG103");
+        System.out.println(ids);
+    }
+}
