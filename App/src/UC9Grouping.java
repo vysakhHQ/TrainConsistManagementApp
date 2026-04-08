6a282da3c02916ce45f37a1e8b4dc4a05fb2bb3a
Vysakh Methalayil <vysakhmethalayil@gmail.com>
2026-04-08 09:19:57 +0530

    'UC9Grouping.java

--- App/src/UC9Grouping.java
+++ App/src/UC9Grouping.java
@@ -1,2 +1,25 @@
-public class UC9Grouping {
+import java.util.*;
+import java.util.stream.*;
+class Bogie{
+String name;
+int capacity;
+Bogie(String name,int capacity){
+this.name=name;
+this.capacity=capacity;
+}
+}
+public class UC9Grouping{
+public static void main(String[] args){
+List<Bogie> list=new ArrayList<>();
+list.add(new Bogie("Sleeper",72));
+list.add(new Bogie("Sleeper",70));
+list.add(new Bogie("AC Chair",54));
+list.add(new Bogie("First Class",24));
+Map<String,List<Bogie>> map=list.stream().collect(Collectors.groupingBy(b->b.name));
+for(String k:map.keySet()){
+System.out.print(k+" ");
+for(Bogie b:map.get(k)){
+System.out.print(b.capacity+" ");
+}
+System.out.println();
 }
