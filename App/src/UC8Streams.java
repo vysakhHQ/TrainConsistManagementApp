80f81a3a167168d88421866290a1b954607e4c2c
VysakhHQ <mvysakh007@gmail.com>
2026-04-08 08:35:30 +0530

    UC8 final update

--- App/src/UC8Streams.java
+++ App/src/UC8Streams.java
@@ -12,11 +12,12 @@ public class UC8Streams{
     public static void main(String[] args){
         List<Bogie> list=new ArrayList<>();
         list.add(new Bogie("Sleeper",72));
-        list.add(new Bogie("AC Chair Car",54));
+        list.add(new Bogie("AC Chair",54));
         list.add(new Bogie("First Class",24));
         List<Bogie> result=list.stream().filter(b->b.capacity>60).toList();
         for(Bogie b:result){
             System.out.println(b.name+" "+b.capacity);
         }
+        System.out.println("UC8 done");
     }
 }
